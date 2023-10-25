7fdf6cdb8b9f6d6e7ff0c07520e44757ef35f54a
Angel Luis Santana Reyes <angellsant@hotmail.com>
2023-10-25 20:25:00 +0100

    commit

--- OrdenacionEdades/src/Main.java
+++ OrdenacionEdades/src/Main.java
@@ -1,10 +1,89 @@
-// Estoy cambiando cosas en el repositorio
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("me llamo luis");
+import java.util.Scanner;
 
+public class OrdenacionEdades {
+
+
+    public static void main(String[] args) {
+
+        int a1, a2, a3;
+        String n1, n2, n3;
+        int e1, e2, e3;
+
+        String maxName, mediumName, minName;
+        int maxAge, mediunAge, minAge;
+
+        int actualYear = 2024;
+
+
+        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("ORDENACIÓN DE EDADES");
+
+        System.out.println("Por favor, dame nombre 1: ");
+        n1 = sc.nextLine();
+        System.out.println("Por favor, dame nombre 2: ");
+        n2 = sc.nextLine();
+        System.out.println("Por favor, dame nombre 3: ");
+        n3 = sc.nextLine();
+
+        System.out.println("Ahora años de nacimiento. ");
+        System.out.println("Por favor, el año de nacimiento de: " + n1);
+        a1 = sc.nextInt();
+        System.out.println("Por favor, el año de nacimiento de: " + n2);
+        a2 = sc.nextInt();
+        System.out.println("Por favor, el año de nacimiento de: " + n3);
+        a3 = sc.nextInt();
+
+        // Calculamos las edades:
+        e1 = actualYear-a1;
+        e2 = actualYear-a2;
+        e3 = actualYear-a3;
+
+        // Ordenación de Mayor a menor
+        if (e1>=e2) {
+            maxAge = e1;
+            maxName = n1;
+
+            if (e2>=e3){
+                mediunAge = e2;
+                mediumName = n2;
+
+                minName = e3;
+                minAge  = e3;
+            }
+            else {
+                mediunAge = e3;
+                mediumName = n3;
+
+                minName = e2;
+                minAge  = e2;
+            }
+
+        }
+        else {
+            maxAge = e2;
+            maxName = n2;
+
+            if (e1>=e3){
+                mediunAge = e1;
+                mediumName = n1;
+
+                minName = e3;
+                minAge  = e3;
+            }
+            else {
+                mediunAge = e3;
+                mediumName = n3;
+
+                minName = e1;
+                minAge  = e1;
+            }
+        }
+
+        System.out.println(maxName + " con " + maxAge + "años");
+        System.out.println(mediumName + " con " + mediunAge + "años");
+        System.out.println(minName + " con " + minAge + "años");
 
     }
 }
